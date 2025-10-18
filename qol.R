d7bf4c5c30869a2d6c2f03f79b817881672280cd
jarogumulec <j.gumulec@gmail.com>
2025-10-18 17:49:38 +0200

    cleanup

--- qol.R
+++ qol.R
@@ -8,13 +8,13 @@ library("gplots")
 library(readxl)
 library(mixOmics)
 
-install.packages("BiocManager") 
-BiocManager::install('mixOmics')
+#install.packages("BiocManager") 
+#BiocManager::install('mixOmics')
 
 # data load  ---------------
 
 # sorking directory - modify accordingly
-setwd("C:/Users/admin/OneDrive - MUNI/Kody/qualityoflife")
+#setwd("C:/Users/admin/OneDrive - MUNI/Kody/qualityoflife")
 setwd("~/OneDrive - MUNI/Kody/QualityOfLife")
 
 qualityoflife_country_compare_2021 <- read_excel("qualityoflife_country_compare_2021.xlsx")
@@ -37,13 +37,13 @@ lgnd = paste(qualityoflife_country_compare_2021$Země, sep=" ")
 scaled <- scale(qualityoflife_country_compare_2021[,2:ncol(qualityoflife_country_compare_2021)])
 input <-  as.matrix(t(scaled))
 
-
+heatmap <-
 heatmap.2(input, trace="none", density="none",  col=bluered(255),
           scale="none",   Colv = T, dendrogram = "both",
           labCol = legend, key = F,  
           keysize = 1, margins = c(6,15))
 
-
+heatmap
 # bez prirody a bez duplicity ceny mzda --------------------------------
 
 scaled <- scale(qualityoflife_country_compare_2021[,c(2:5, 7:9)])
@@ -68,7 +68,7 @@ plotIndiv(principalcomp, comp = c(1, 2),
 plotVar(principalcomp, comp = c(1, 2), cex = 2.1,
           legend = F, title = 'Quality of life, PCA')
 
-#biplot(principalcomp, cex = 0.7, xlabs = lgnd)
+biplot(principalcomp, cex = 0.7, xlabs = lgnd)
 
 # identiciface jak dulezite ty faktory jsou
 # scree plot přímo z mixOmics
